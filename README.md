5062bdcf56b07016f6e16bf0e5ea9f3d809e92e4
Lucas Hawes <doubletigerstripe@gmail.com>
2018-04-27 16:47:24 -0700

    Init, working through example

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # jiraDescExtender
 A Jira description extender plugin for google chrome
+
+https://developer.chrome.com/extensions/getstarted
---
+++ background.js
@@ -0,0 +1,6 @@
+// console.log('hello world');
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({color: '#3aa757'}, function() {
+      console.log("The color is green.");
+    });
+  });
